7b526a984d3a2455ca5fbc560e0dce4c6861c48e
hristoil <h.chorbadzhiyski@gmail.com>
2018-01-12 12:47:54 +0200

    Working

---
+++ SubstringUse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EscapeCharsFromString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "LEGO STAR WARS. The Force Awakens";
+
+            int index = str.IndexOf('.');          
+
+            Console.WriteLine(str.Substring(index+2));
+
+
+        }
+    }
+}
