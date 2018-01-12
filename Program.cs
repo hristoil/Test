b22d8d125e5c52fbbdec0503bc3a9582b90e3f9f
hristoil <h.chorbadzhiyski@gmail.com>
2018-01-12 12:12:37 +0200

    Count String Appereances

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            string pattern = Console.ReadLine();
+
+            int count = 0;
+
+            int index = text.IndexOf(pattern);
+
+            while (index != -1) 
+            {
+                count++;
+                index = text.IndexOf(pattern, index + 1);
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
